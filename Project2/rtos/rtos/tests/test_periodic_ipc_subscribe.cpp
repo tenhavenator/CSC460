c7128e68497d8cdf6a5336e9f8af2229bd7df094
unknown <tenhavenator@gmail.com>
2015-03-07 17:07:23 -0800

    Added files for ipc testing

--- Project2/rtos/rtos/tests/test_periodic_ipc_subscribe.cpp
+++ Project2/rtos/rtos/tests/test_periodic_ipc_subscribe.cpp
@@ -1,12 +1,11 @@
 #include "../os.h"
 #include <avr/io.h>
-#include <util/delay.h>
 
 /* To run this test, comment out the original function declaration and uncomment the r_main one. 
  * There can only be one r_main declared at any time.
  */
 // int r_main(void)
-int r_main_test_too_many_subscribers(void)
+int r_main_test_periodic_ipc_subscribe(void)
 {    	
 	return 0;
 }
