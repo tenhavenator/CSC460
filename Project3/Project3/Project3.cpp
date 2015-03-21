49b82c3cad3da611416db94a49d86b6855699e8a
unknown <tenhavenator@gmail.com>
2015-03-21 13:46:41 -0700

    basic roomba moving

--- Project3/Project3/Project3.cpp
+++ Project3/Project3/Project3.cpp
@@ -6,29 +6,40 @@
 
 
 #include "OS/os.h"
+#include "roomba/roomba.h"
+#include "roomba/roomba_sci.h"
+
 #include <avr/io.h>
 
 int r_main(void)
 {
-	DDRB = _BV(7);
+	Roomba_Init();
 	
-    while(1)
-    {
-		uint16_t start = Now();
-		PORTB = _BV(7);
-		
-		while(Now() - start < 1000)
-		{
-		
-		}
-		
-		PORTB = 0;
-		
-		while(Now() - start < 2000)
-		{
-			
-		}
-    }
+	uint16_t start = Now();
 	
+	Roomba_Drive(250, 0x8000);
+	
+	while(Now() - start < 2000)
+	{
+		
+	}
+
+	Roomba_Drive(500, 0x8000);
+	
+	while(Now() - start < 3500)
+	{
+		
+	}
+	
+	Roomba_Drive(250, 0x8000);
+	
+	while(Now() - start < 5500)
+	{
+		
+	}
+	
+	Roomba_Drive(0, 0x8000);
+	
+	Task_Terminate();
 	return 0;
 }
