229644107130ef22843a5e8ae61be1af203a533c
unknown <tenhavenator@gmail.com>
2015-03-21 14:29:47 -0700

    Roomba receives basic drive commands

--- Project3/Project3/Project3.cpp
+++ Project3/Project3/Project3.cpp
@@ -17,23 +17,16 @@ int r_main(void)
 	
 	uint16_t start = Now();
 	
-	Roomba_Drive(250, 0x8000);
+	Roomba_Drive(50, 0x8000);
 	
-	while(Now() - start < 2000)
+	while(Now() - start < 5000)
 	{
-		
 	}
 
 	Roomba_Drive(500, 0x8000);
 	
-	while(Now() - start < 3500)
-	{
-		
-	}
-	
-	Roomba_Drive(250, 0x8000);
-	
-	while(Now() - start < 5500)
+	start = Now();
+	while((Now() - start < 5000)
 	{
 		
 	}
